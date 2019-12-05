a947f19413b469c16e102a26fb479319c93df61e
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2019-12-05 18:06:19 +0300

    Updated Source.cpp
    Made more specific error-displaying solutions. Made the program the way it is wanted

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
+#include <iostream>	// simple input output library
+#include <fstream> // to be able to open files.
+#include <vector> // If the don't know the size of an array beforehand, it is better to use vectors. Because their size is dynamic
+#include <string> // to be able to use strings
 #include <chrono> // to measure the elapsed time
 using namespace std;
 
 int findString(string main, string sub)
-{
+{ //main is the script string, sub is the string that is to be searched
 	int M = main.size();
 	int N = sub.size();
 
@@ -19,15 +18,16 @@ int findString(string main, string sub)
 			if (main[i + j] != sub[j])
 				break;
 		}
+		//If the inner loop if condition is never reached, algorithm can reach here
 		if (j == N)
 			return i;
 	}
-
+	// If no value is returned so far, that means nothing is found. Return an error value.
 	return -1;
 }
 
 int findString(string main, string sub, int index)
-{
+{ //main is the script string, sub is the string that is to be searched
 	int M = main.size();
 	int N = sub.size();
 
@@ -39,23 +39,27 @@ int findString(string main, string sub, int index)
 			if (main[i + j] != sub[j])
 				break;
 		}
+		//If the inner loop if condition is never reached, algorithm can reach here
 		if (j == N)
 			return i;
 	}
-
+	// If no value is returned so far, that means nothing is found. Return an error value.
 	return -1;
 }
 
 int main()
 {
+	//Start measuring the time
 	auto start = chrono::high_resolution_clock::now();
 
-	ifstream statementsFile("statements.txt"); //Read from the statements.txt
+	//Read from the statements.txt
+	ifstream statementsFile("statements.txt");
 
-	string line, word;
+	string line;
 	vector<string> statements;
 	if (!statementsFile.good())
 	{
+		//Display error and exit the program
 		cout << "Unable to read the statements file! The file name has to be \"statements.txt\" (without quatition marks)\n";
 		return 0;
 	}
@@ -72,43 +76,54 @@ int main()
 	vector<size_t> statementsUnderScoreIndex; //This Index will store the location of the first underscore in statements.
 	for (int i = 0; i < statements.size(); i++)
 	{
-		statementsUnderScoreIndex.push_back(statements[i].find("___"));
-
+		statementsUnderScoreIndex.push_back(findString(statements[i],"___")); //Find the starting indexes of "___"s
 	}
 
 	//Read script file
-	ifstream scriptFile("the_truman_show_script.txt"); // to read from the file
+	ifstream scriptFile("the_truman_show_script.txt");
 	if (!scriptFile.good())
 	{
+		//Display error and exit the program
 		cout << "Unable to read the script file! The file name has to be \"the_truman_show_script.txt\" (without quatition marks)\n";
 		return 0;
 	}
-	while (scriptFile.good())
+
+	//Since the script file contains only one line, we read it only once.
+	if (scriptFile.good())
 	{
-		getline(scriptFile, line, '\n');
+		getline(scriptFile, line);
 	}
-
+	scriptFile.close();
+	
+	//Search for each and every statement through the script file
 	for (int i = 0; i < statements.size(); i++) {
-		string word;
-		bool firstPassed = false;
-		string firstString = statements[i].substr(0, statementsUnderScoreIndex[i]); // before ___ including " "
-		string nextString = statements[i].substr(statementsUnderScoreIndex[i] + 3); // after ___ including " "
-		int iterations = line.size();
-
+		string firstString, nextString, word;
+		firstString = statements[i].substr(0, statementsUnderScoreIndex[i]); // before ___ including " "
+		nextString = statements[i].substr(statementsUnderScoreIndex[i] + 3); // after ___ including " "
 
 		int first, last;
-		first = findString(line, firstString) + firstString.size(); // first = line.find(firststring) + firststring.size();
-		last = findString(line, nextString, first); // last = line.find(nextstring, first);
-		word = line.substr(first, last - first);
-		cout << i + 1 << ") " << statements[i] << '\n' << word << "\n";
+		first = findString(line, firstString) + firstString.size(); // find the first character index of the missing word
+
+		if (first == (firstString.size() - 1)) { //Check if the statement exists
+			cout << i + 1 << ") " << statements[i] << '\n' << "Statement NOT found\n";
+			continue;
+		}
+
+		last = findString(line, nextString, first); // find the last character index of the missing word
+
+		if (last == -1) { //Check if the statement exists
+			cout << i + 1 << ") " << statements[i] << '\n' << "Statement NOT found\n";
+			continue;
+		}
+
+		word = line.substr(first, last - first); // Take a substring of the word from script file
+		cout << i + 1 << ") " << statements[i] << '\n' << word << "\n"; //Display the statement and it's missing word
 	}
 
+	//Display the elapsed time
 	auto finish = chrono::high_resolution_clock::now();
-
 	chrono::duration<double> elapsed = finish - start;
-
 	cout << "\nElapsed time: " << elapsed.count() << " seconds\n";
-
 	cout << "Press Enter to finish the program";
 	cin.get();
 
